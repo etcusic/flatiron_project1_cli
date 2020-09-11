04b672180b63f2313b9f3eb6c5d9d43371ee6fdf
Eli Cusic <tntitans31@gmail.com>
2020-09-11 14:29:29 -0500

    Final iteration without an API.

--- bin/executable.rb
+++ bin/executable.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/env ruby
-require_relative '../config/cli.rb'
+
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
+require_relative '../lib/flow_module.rb'
+require_relative '../lib/cli.rb'
+require_relative '../lib/league_module.rb'
+require_relative '../lib/league.rb'
+require_relative '../lib/team.rb'
+require_relative '../lib/player.rb'
+
 
 nfl = League.new({name: "NFL", url: "https://www.nfl.com/network/"})
 
 Interaction.new(nfl)
-
